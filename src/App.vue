30aab123f9fcaf5a4a9feaf90875c5def72d0fe2
manzanillo <manza@outlook.de>
2019-04-08 17:43:03 +0200

    added sample for moves

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
 <template>
   <div id="app">
     <h1>Reinforcement Learning - Schlag das Krokodil</h1>
-    <Chess v-bind:state="[1,1,1,0,0,0,2,2,2]"/>
+    <div id="main-game">
+      <Chess v-bind:state="[1,1,1,0,0,0,2,2,2]"/>
+    </div>
+
+    <!-- Da meistens 16:9 Monitore verwendet werden, sollte das vermutlich rechts vom Spielfeld angezeigt werden -->
+    <h4>Beispiel für kleineres Schach, um Spielsituation zu zeigen</h4>
+    <div class="rulesets">
+      <Chess v-bind:state="[0,1,1,1,0,0,2,2,2]"/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
 import Chess from "./components/chess.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld,
     Chess
   }
 };
@@ -26,5 +32,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.rulesets {
+  width: 20%;
+}
+
+#main-game {
 }
 </style>
