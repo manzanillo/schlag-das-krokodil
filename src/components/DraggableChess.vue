a0b0a831dd4ed5b866c52ad799b94dd04ca8f6dd
manzanillo <manza@outlook.de>
2019-04-11 13:43:59 +0200

    drag n drop now works correctly

--- src/components/DraggableChess.vue
+++ src/components/DraggableChess.vue
@@ -1,30 +1,30 @@
 <template id="chessground">
-  <div>
-    <draggable
-      class="field"
-      v-model="currentState"
-      :move="checkMove"
-      :options="{disabled: !usersTurn}"
+  <div class="field">
+    <div
+      v-for="(occupation, index) in currentState"
+      :key="index"
+      v-drag-and-drop
+      v-on:drag="handleDrag"
+      v-on:drop="handleDrop"
+      v-on:drag-over="handleDragOver"
     >
-      <div v-for="(occupation, index) in currentState" :key="index">
-        <div class="field-slot" v-if="occupation === 0">
-          <!-- field is empty -->
-        </div>
-        <div class="field-slot" v-if="occupation === 1">
-          <!-- player is occupying field -->
-          <img src="../assets/monkey.svg" alt="triangle with all three sides equal" width="100%">
-        </div>
-        <div class="field-slot" v-if="occupation === 2">
-          <!-- computer is occupying field -->
-          <img src="../assets/croco.svg" alt="triangle with all three sides equal" width="100%">
-        </div>
+      <div class="field-slot" v-if="occupation === 0">
+        <!-- field is empty -->
+        <img v-bind:id="index" src="../assets/white.svg" alt="empty" width="100%">
       </div>
-    </draggable>
+      <div class="field-slot" v-if="occupation === 1">
+        <!-- player is occupying field -->
+        <img v-bind:id="index" src="../assets/monkey.svg" alt="human" width="100%">
+      </div>
+      <div class="field-slot" v-if="occupation === 2">
+        <!-- computer is occupying field -->
+        <img v-bind:id="index" src="../assets/croco.svg" alt="computer" width="100%">
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import draggable from "vuedraggable";
 import {
   calculatePossibleMoves,
   arrayContainsArray,
@@ -33,34 +33,39 @@ import {
 
 export default {
   name: "DraggableChess",
-  components: {
-    draggable
-  },
+  components: {},
   props: {
     state: Array,
     usersTurn: Boolean
   },
   data() {
     return {
-      currentState: this.state
+      currentState: this.state,
+      currentlyDragging: null,
+      loggedEvent: ""
     };
   },
   methods: {
-    checkMove: function(evt) {
-      if (this.currentState[evt.draggedContext.index] == 1) {
-        //player selected
-        const possibleMoves = calculatePossibleMoves(this.currentState, 1);
-        const moveTried = [evt.draggedContext.index, evt.relatedContext.index];
-        if (arrayContainsArray(possibleMoves, moveTried)) {
-          this.currentState[evt.oldIndex] = 0;
-          this.currentState = performMove(this.currentState, moveTried);
-          this.$forceUpdate();
-          this.$emit("new-state", this.currentState);
-
-          return false; //workaround ;-)
-        }
+    handleDrop: function(e) {
+      console.log("handleDrop", this.currentlyDragging, e.target);
+      const from = this.currentlyDragging.id;
+      const to = e.target.id;
+      const placeholder = this.currentState[from];
+      const possibleMoves = calculatePossibleMoves(this.currentState, 1);
+      const moveTried = [from, to];
+      if (arrayContainsArray(possibleMoves, moveTried)) {
+        this.currentState[this.currentlyDragging.id] = e.target.id;
+        this.currentState[e.target.id] = placeholder;
+        this.$forceUpdate();
+        this.$emit("new-state", this.currentState);
       }
-      return false;
+    },
+    handleDrag: function(e) {
+      this.currentlyDragging = e.srcElement;
+    },
+    handleDragOver: function(e) {
+      console.log("handleDragOver", e);
+      this.loggedEvent = "handleDragOver";
     }
   },
   watch: {
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Message from 'vue-m-message'
+import DragAndDrop from 'vue-drag-and-drop'
 
 Vue.config.productionTip = false
 Vue.use(Message) // will mount `Vue.prototype.$message`
+Vue.use(DragAndDrop)
 
 new Vue({
   render: h => h(App)
